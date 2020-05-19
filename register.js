0f2f9b01f21ffc804230c11e3e4c20c3e4421de2
scottbarnesg <scott.barnes.g@gmail.com>
2020-05-19 18:16:14 -0400

    More more bug fixes

--- register.js
+++ register.js
@@ -24,36 +24,15 @@ function sendRegisterRequest(){
   payload['country'] = country;
   payload['dateOfBirth'] = dob;
   // Send POST request
-  var xhr = new XMLHttpRequest();
   var url = "https://nexgame.hopto.org:58443/api/register";
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-          var response = JSON.parse(xhr.responseText);
-          // If error, display useful message
-          if (response.status==="ERROR"){
-            if (response.error==="USER EXISTS"){
-              document.getElementById("response").innerHTML = "ERROR: That username is already taken.";
-              return -1;
-            }
-            if (response.error==="EMAIL EXISTS"){
-              document.getElementById("response").innerHTML = "ERROR: That email is already in use.";
-              return -1;
-            }
-            document.getElementById("response").innerHTML = "ERROR: Something went wrong. Try again";
-            return -1;
-          }
-          // If OK, redirect to "Welcome" page
-          if (response.status="OK"){
-            document.getElementById("response").innerHTML = "SUCCESS! Welcome to NexGame.";
-            return 0;
-          }
-      }
-      else if (xhr.readyState === 4){
-        alert(xhr.status);
-      }
-  };
-  xhr.open("POST", url, true);
-  var data = JSON.stringify(payload);
-  xhr.send(data);
+  fetch(url, {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  }).then(res=>res.json())
+    .then(res => console.log(res));
+  alert(res);
 }
