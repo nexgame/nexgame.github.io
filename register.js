d9286c332b6c07380b35b3f5492c3869e9166ce8
scottbarnesg <scott.barnes.g@gmail.com>
2020-06-08 18:29:54 -0400

    Update signup page

--- register.js
+++ register.js
@@ -4,13 +4,34 @@ async function sendPostRequest(){
   var lastname = document.getElementById("lastname").value;
   var username = document.getElementById("username").value;
   var email = document.getElementById("email").value;
-  var dob = document.getElementById("dob").value;
+  // var dob = document.getElementById("dob").value;
   var password = document.getElementById("password").value;
   var verifypw = document.getElementById("verifypw").value;
-  var country = document.getElementById("country").value;
+  // var country = document.getElementById("country").value;
   // Make sure passwords match
   if (password.trim() !== verifypw.trim()){
     document.getElementById("response").innerHTML = "ERROR: Password mismatch.";
+    return -1;
+  }
+  // Make sure inputs are valid
+  if (firstname.trim().length() < 1){
+    document.getElementById("response").innerHTML = "ERROR: Please enter your first name.";
+    return -1;
+  }
+  if (lastname.trim().length() < 1){
+    document.getElementById("response").innerHTML = "ERROR: Please enter your last name.";
+    return -1;
+  }
+  if (username.trim().length() < 3){
+    document.getElementById("response").innerHTML = "ERROR: Your username must be at least 3 characters.";
+    return -1;
+  }
+  if (!email.includes("@")){
+    document.getElementById("response").innerHTML = "ERROR: Please provide a valid email address.";
+    return -1;
+  }
+  if (password.trim().length() < 8){
+    document.getElementById("response").innerHTML = "ERROR: Your password must be at least 8 characters.";
     return -1;
   }
   // Format POST request
@@ -21,8 +42,8 @@ async function sendPostRequest(){
   payload['firsname'] = firstname;
   payload['lastname'] = lastname;
   payload['gender'] = 'Q';
-  payload['country'] = country;
-  payload['dateOfBirth'] = dob;
+  payload['country'] = 'USA';
+  payload['dateOfBirth'] = '1/22/1969';
   // Send POST request
   var url = "https://nexgame.hopto.org:58443/api/register";
   const response = await fetch(url, {
@@ -36,7 +57,7 @@ async function sendPostRequest(){
   const json = await response.json();
   if (json.status === "ERROR"){
     if (json.error === "USER EXISTS"){
-      document.getElementById("response").innerHTML = "ERROR: Username already exists.";
+      document.getElementById("response").innerHTML = "ERROR: That username already taken.";
       return -1;
     }
     if (json.error === "EMAIL EXISTS"){
