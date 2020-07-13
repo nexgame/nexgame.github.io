3283d4cbb34ba41fe24cb5ce033ed0d5a2dca3b1
scottbarnesg <scott.barnes.g@gmail.com>
2020-07-12 22:18:24 -0400

    Added setup page

--- write-text.js
+++ write-text.js
@@ -6,10 +6,10 @@ function writeText(){
   if (i < message.length){
     document.getElementById("typedTitle").innerHTML += message.charAt(i);
     if (message.charAt(i) == ".") {
-      delay = 150;
+      delay = 200;
     }
     else {
-      delay = 60;
+      delay = 70;
     }
     i++;
     setTimeout(writeText, delay);
